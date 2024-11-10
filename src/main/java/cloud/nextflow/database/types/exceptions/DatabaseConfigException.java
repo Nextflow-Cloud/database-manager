a81ac2945f39ac0f752857dd20942f1d137fb300
Queryzi <56193446+Queryzi@users.noreply.github.com>
2024-11-10 13:48:20 -0500

    add: add type safety for parameters

---
+++ src/main/java/cloud/nextflow/database/types/exceptions/DatabaseConfigException.java
@@ -0,0 +1,15 @@
+package cloud.nextflow.database.types.exceptions;
+
+public class DatabaseConfigException extends DatabaseException {
+    public DatabaseConfigException(Throwable cause) {
+        super(cause);
+    }
+
+    public DatabaseConfigException(String message) {
+        super(message);
+    }
+
+    public DatabaseConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
